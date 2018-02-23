f3ac2b4a2a1ab09334baf871c5200906b9de2ab4
Oskar540 <xmrplayerowyx@gmail.com>
2018-02-23 17:56:54 +0100

    Drag mechanic
    Added puzzle drag mechanic.

---
+++ DEV/Assets/scripts/PlaceMagnet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceMagnet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log(collision.name + " magneted by: " + this.name);
+        collision.GetComponent<Transform>().position = (new Vector3(this.transform.position.x, this.transform.position.y, collision.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ DEV/Assets/scripts/puzzleController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzleController : MonoBehaviour {
+
+    
+    float x;
+    float y;
+    public float speed = 1.0f;
+    bool isLocked;
+    public Collider2D correctPlace;
+    //public AnimationClip anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        isLocked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.mousePosition.x;
+        y = Input.mousePosition.y;
+        //if (isGameOver())
+        {
+            //Debug.Log("GameOver!");
+        }
+    }
+    void OnMouseDrag()
+    {
+        if (!isLocked)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 16.01f));
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if ((collision == correctPlace) && !Input.GetMouseButtonDown(0))
+        {
+            this.transform.position = collision.transform.position;
+            isLocked = true;
+        }
+    }
+
+    //bool isGameOver()
+    //{
+    //    foreach (var item in puzzles.puzzlesList)
+    //    {
+    //        if (!item.isLocked) return false;
+    //    }
+    //    return true;
+    //}
+
+}
